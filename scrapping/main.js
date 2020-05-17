cf20a4cdaeefc791392da522c1117fea94cf5334
Jesus Angel <jesus.angel.9703@gmail.com>
2020-05-16 19:02:57 -0500

    Screenshot is now formatted with date

--- scrapping/main.js
+++ scrapping/main.js
@@ -1,14 +1,30 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
-(async () => {
-  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+takeScreenshot = async (date) => {
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
   const page = await browser.newPage();
   await page.setViewport({
     width: 2000,
     height: 2000,
     deviceScaleFactor: 1,
   });
-  await page.goto('https://www.google.com/maps/@4.6528093,-74.0863529,13z/data=!5m1!1e1');
-  await page.screenshot({path: 'screenshot.png'});
+  await page.goto(
+    "https://www.google.com/maps/@4.6528093,-74.0863529,13z/data=!5m1!1e1"
+  );
+  await page.screenshot({ path: `screenshot/${date}.png` });
   await browser.close();
-})();
+};
+const now = new Date();
+var timestamp =
+  now.getFullYear() +
+  "_" +
+  now.getMonth() +
+  "_" +
+  now.getDate() +
+  "_" +
+  now.getHours() +
+  "_" +
+  now.getMinutes();
+takeScreenshot(timestamp);
